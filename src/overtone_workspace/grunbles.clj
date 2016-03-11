4d4cd55afc9a79423c6065d0a854ec9932c0b044
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-03-11 11:51:16 +0900

    Set tone duration to 8 bars

--- src/overtone_workspace/grunbles.clj
+++ src/overtone_workspace/grunbles.clj
@@ -3,10 +3,10 @@
 
 ;; Inspired by an example in an early chapter of the SuperCollider book
 
-(definst grumble [speed 6 freq-mul 1]
+(definst grumble [speed 6 freq-mul 1 attack 10 release 50]
   (let [snd (mix (map #(* (lf-tri (* % freq-mul 100))
                           (max 0 (+ (lf-noise1:kr speed)
-                                    (env-gen (perc 20 100) :action FREE))))
+                                    (env-gen (perc attack release) :action FREE))))
                       [1 (/ 2 3) (/ 3 2) 2]))]
     (pan2 snd (sin-osc:kr 0.125))))
 
@@ -19,23 +19,26 @@
 ;; (grumble :freq-mul 2)
 ;; (ctl grumble :speed 3000)
 
-(volume (/  4  127))
+(volume (/  10  127))
 
 (def metro (metronome 128))
 
-(defn player [beat notes]
-  (let [notes (if (empty? notes)
-                [4 8 2 1.5]
-                notes)]
+(defn player [beat rates]
+  (let [rates (if (empty? rates)
+                [4 8 2 2 1.5 1.5]
+                rates)
+        bpm (metro :bpm)
+        dur (/ 60.0 bpm)
+        attack (* dur 4) ; 1bar
+        rel (* dur (* 4 7))]  ; 7bars
     (at (metro beat)
         (do
-          (if (zero? (mod beat 8)) (grumble :freq-mul 0.5))
-          (if (zero? (mod beat 8)) (grumble :freq-mul 1))
+          (if (zero? (mod beat 8)) (grumble :freq-mul 0.5 :attack attack :release rel))
+          (if (zero? (mod beat 8)) (grumble :freq-mul 1 :attack attack :release rel))
+          (if (zero? (mod beat 16))
+            (grumble :freq-mul (choose rates) :attack attack :release rel))
           ))
-    (at (metro (+ 0.5 beat))
-        (if (zero? (mod beat 16))
-          (grumble :freq-mul (choose notes))))
-    (apply-by (metro (inc beat)) #'player (inc beat) (next notes) [])
+    (apply-by (metro (inc beat)) #'player (inc beat) (next rates) [])
     ))
 
 ;;(player (metro) [])
