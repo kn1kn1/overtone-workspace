5bc873c6232ba8055dd9a8624bfd7f3e27047422
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-03-16 11:01:19 +0900

    Change to use layers

--- src/overtone_workspace/core.clj
+++ src/overtone_workspace/core.clj
@@ -1,54 +1,55 @@
 (ns overtone-workspace.core
   (:require [quil.core :as q]
-            [quil.middleware :as m]
-            [overtone-workspace.voltap :as voltap])
-  (:use [overtone.live]
-        [overtone.synth.stringed])
+            [quil.middleware :as m])
+  (:use [quil-layer layer layers]
+        [quil-layer.layers fadeout-layer layer-example2]
+        [overtone-workspace.layers layer-example vol-elipse-layer])
   )
 
-; (defonce sound-in-synth3000 (voltap/sound-in-vol-tap 3000))
-(defonce sound-in-synth3000 (voltap/vol-tap [:after (foundation-monitor-group)] 3000))
-(def circle-num 10)
-
 (defn setup []
+  ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  {:vol 0
-   :bgcolour 0
-   :radius 10})
+  ; Set color mode to HSB (HSV) instead of default RGB.
+  ;(q/color-mode :rgb)
+  (q/color-mode :hsb)
+  ; (q/background 128 255 255)
+  (q/background 128)
+)
 
 (defn update-state [state]
-                                        ;  (q/exit)
-  ;(q/frame-rate 30)
-  (let
-    [vol (voltap/monitor-vol)
-     vol3000 (voltap/monitor-vol sound-in-synth3000)]
-    {:vol vol
-     :bgcolour (* 10 (* 10 vol))
-     :radius (+ 10 (* 10000 vol3000))}))
+  (update-layers)
+  state)
 
 (defn draw-state [state]
-  (let [bgcolour (:bgcolour state)
-        radius (:radius state)
-        circle-num (* 5 (:vol state))
-        r (+ (* 20 bgcolour) 120)
-        g (+ (* 20 bgcolour) 30)
-        b (+ (* 20 bgcolour) 60)
-        w (q/width)
-        h (q/height)
-        fw (/ w 2)
-        fh (/ h 2)]
-    (q/background r g b)
-    (dorun
-      (for [i (range 0 circle-num)]
-        (let [x (- (/ fw 2) (rand fw))
-              y (- (/ fh 2) (rand fh))
-              r (* (rand 2) radius)]
-          (q/stroke-int (- 255 bgcolour))
-          (q/no-fill)
-          (q/with-translation [(/ w 2)
-                               (/ h 2)]
-            (q/ellipse x y r r)
-            ))))))
+  (draw-layers))
+
+(comment
+  (do
+    (def layerex (->LayerExample (atom {})))
+    (let [layer layerex]
+      (setup-layer layer)
+      (add-layer layer)))
+  (remove-layer layerex)
+  )
+
+(comment
+  (do
+    (def layerex2 (->LayerExample2 (atom {})))
+    (let [layer layerex2]
+      (setup-layer layer)
+      (add-layer layer)))
+  (remove-layer layerex2)
+  )
+
+(comment
+  (do
+    (def fadeoutlayer (->FadeoutLayer 5000 (atom {})))
+    (let [layer fadeoutlayer]
+      (setup-layer layer)
+      (add-layer layer)))
+  (println fadeoutlayer)
+  (remove-layer fadeoutlayer)
+  )
 
 (q/defsketch voltap
   :title "You spin my circle right round"
