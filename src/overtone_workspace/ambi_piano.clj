c9d170b32b8ba3d4b7c8805c0c26faa9833de00a
kn1kn1 <kn1kn1@users.noreply.github.com>
2016-05-11 17:56:39 +0900

    Revise ambi_piano.clj

--- src/overtone_workspace/ambi_piano.clj
+++ src/overtone_workspace/ambi_piano.clj
@@ -5,7 +5,7 @@
 ;; loop{use_bpm 70;sample:ambi_piano,sustain:0.25,rate:pitch_to_ratio([-5,-2,0,3].choose)*[1,2,4].choose*[1,-1].choose,start:rand;sleep 0.25}
 
 (def ambpf-b (load-sample "resources/ambi_piano.wav"))
-(definst ambpf [start 0 rate 1.0 dur 0.25 amp 10]
+(definst ambpf [start 0 rate 1.0 dur 0.25 amp 1]
   (let [frames (num-frames ambpf-b)
         trig (env-gen:kr (lin 0 dur 0))
         src (play-buf 1 ambpf-b rate trig (* frames start) 0 FREE)
@@ -14,19 +14,21 @@
 ;;(ambpf (rand) (rand) 0.25)
 ;;(odoc load-sample)
 
-(defn exponential [x n]
+(defn- exponential [x n]
   (reduce * (repeat n x)))
-(defn pitch-to-ratio [m]
+(defn- pitch-to-ratio [m]
   (exponential 2.0 (/ m 12.0)))
 (defn ambpf-player [metro beat]
   (let [start (rand)
         rate (* (pitch-to-ratio (choose [-5 -3 0 2])) (choose [1 2 4]))
-        dur (/ 60 (metro :bpm))]
+        dur (/ 60 (metro :bpm))
+        next-beat (+ beat 1/2)]
     (at (metro beat) (ambpf start rate (/ dur 2)))
-    (apply-by (metro (+ beat 1/2)) #'ambpf-player [metro (+ beat 1/2)])))
+    (apply-by (metro next-beat) ambpf-player metro next-beat [])))
 
 (comment
   (do
+    (volume 0.6)
     (def metro (metronome 128))
     (ambpf-player metro (metro)))
   (stop)
